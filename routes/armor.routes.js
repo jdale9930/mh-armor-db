c825a984b28083b44536a1351d68324388066400
Joshua Dale <jdale9930@gmail.com>
2021-03-25 13:30:10 -0500

    added search to decorations

--- routes/armor.routes.js
+++ routes/armor.routes.js
@@ -7,4 +7,28 @@ router.post("/add", (req,res)=>{
     return armor.add(res, req.body)
 })
 
+router.get("/all", (req, res)=>{
+    return armor.all(res);
+})
+
+
+router.get("/search", (req, res)=>{
+    return armor.search(res, req.query.name, req.query.skill, req.query.slots, req.query.type)
+})
+
+router.get("/bySkill/:skill", (req, res)=>{
+    return armor.bySkill(res, req.params.skill)
+})
+
+router.get("/bySlots/:Slots", (req, res)=>{
+    return armor.bySlots(res, req.params.Slots)
+})
+
+router.get("/byType/:Type", (req, res)=>{
+    return armor.byType(res, req.params.Type)
+})
+
+router.get("/byClass/:Class", (req, res)=>{
+    return armor.byClass(res, req.params.Class)
+})
 module.exports = router
