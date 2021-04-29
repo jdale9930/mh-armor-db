182cfb976eb0f7b1e5598f585422cf6d9bd68f36
Joshua Dale <jdale9930@gmail.com>
2021-04-29 12:13:59 -0500

    added build file

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require("dotenv").config()
 const express = require("express")
 const session = require("express-session")
 const app = express()
+app.use(express.static(__dirname + "/build"));
 
 app.use(session({secret: "It's a secret", resave: false, saveUninitialized: true}))
 
-const port = process.env.PORT || 9090
+const port = process.env.PORT || 8080
 const armorRoutes = require("./routes/armor.routes")
 const decorationRoutes = require("./routes/decoration.routes")
 const skillsRoutes = require("./routes/skills.routes")
@@ -24,5 +25,9 @@ app.use("/skills", skillsRoutes)
 app.use("/user", userRoutes)
 app.use("/talisman", talismanRoutes)
 app.use("/armorset", armorsetRoutes)
+
+app.get("*", (req, res) => {
+    res.sendFile("/build/index.html", { root: __dirname + "/" });
+  });
 
 app.listen(port, ()=> console.log(`Huzzah, we're on port ${port}`))
